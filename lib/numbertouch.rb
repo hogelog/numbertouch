bf95dcf9d1fba2f349c7a0e553f969b8cf7e4af5
hogelog <konbu.komuro@gmail.com>
2022-02-02 00:01:58 +0900

    bundle gem numbertouch

---
+++ lib/numbertouch.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "numbertouch/version"
+
+module Numbertouch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/numbertouch/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Numbertouch
+  VERSION = "0.1.0"
+end
